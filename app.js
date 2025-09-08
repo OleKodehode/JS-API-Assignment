156c8678501d716a0ac0888966f502f2a3c31d9d
Ole Kodehode <ole.kodehode@gmail.com>
2025-09-08 14:56:56 +0200

    added comments to app.js and attribution in index footer

---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+
+Chuck norris IO calls;
+Fetch one random quote:
+(GET) https://api.chucknorris.io/jokes/random 
+
+Fetch one random quote from a category:
+(GET) https://api.chucknorris.io/jokes/random?category={category}
+
+Fetch all categories:
+(GET) https://api.chucknorris.io/jokes/categories
+
+Chuck norris categories:
+animal, career, celebrity,
+dev,explicit, fashion,
+food, history, money,
+movie,music,political,
+religion, science, sport,
+travel.
+
+Text search for quotes:
+(GET) https://api.chucknorris.io/jokes/search?query={query}
+
+Useless facts API:
+https://uselessfacts.jsph.pl
+
+Get today's useless fact;
+(GET) https://uselessfacts.jsph.pl/api/v2/facts/today
+
+Get a random useless fact;
+(GET) https://uselessfacts.jsph.pl/api/v2/facts/random
+
+*/
